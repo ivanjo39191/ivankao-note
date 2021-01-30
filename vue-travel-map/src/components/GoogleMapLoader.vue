f861d70adae231f45a0bc430b905f06d9ef41398
ivanjo39191 <ivanjo39191@gmail.com>
2021-01-30 22:13:53 +0800

    feat: 添加創建標誌功能 調整項目: 1. 新增套件 axios 2. GoogleMapLoader 添加顯示地標按鈕 3. GoogleMapLoader 添加傳入標誌 searchmarker method 4. 新增元件 GoogleMapMarker 5. GoogleMapMarker 添加創建標誌 makemarker method

--- vue-travel-map/src/components/GoogleMapLoader.vue
+++ vue-travel-map/src/components/GoogleMapLoader.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <button @click="searchmarker" class="btn" type="button">顯示地標</button>
     <div class="google-map" ref="googleMap"></div>
     <template v-if="Boolean(this.google) && Boolean(this.map)">
       <slot
@@ -7,13 +8,21 @@
         :map="map"
       />
     </template>
+    <GoogleMapMarker ref="googlemapmarker"
+      :google="google"
+      :map="map"
+    />
   </div>
 </template>
 
 <script>
 import GoogleMapsApiLoader from 'google-maps-api-loader'
+import GoogleMapMarker from '@/components/GoogleMapMarker'
 
 export default {
+  components: {
+    GoogleMapMarker,
+  },
   props: {
     mapConfig: Object,
     apiKey: String,
@@ -36,6 +45,24 @@ export default {
     initializeMap() {
       const mapContainer = this.$refs.googleMap
       this.map = new this.google.maps.Map(mapContainer, this.mapConfig)
+    },
+    searchmarker: function() {
+      const cors = 'https://cors-anywhere.herokuapp.com/';
+      const url = "http://gis.taiwan.net.tw/XMLReleaseALL_public/scenic_spot_C_f.json";
+      this.$axios.get(cors+url)
+      .then(res => {
+          for (const [index, value] of res.data['XML_Head']['Infos']['Info'].slice(1500,1600).entries()){
+            let marker = [];
+            let is_first = false;
+            if (index == 0){is_first = true;}
+            marker = { 
+              id: value['Id'], 
+              position:{ lng: parseFloat(value['Px']), lat: parseFloat(value['Py'])}, 
+            };
+            this.$refs.googlemapmarker.makemarker(marker, is_first);
+          }
+      })
+      .catch(err => console.log(err));
     }
   }
 }
---
+++ vue-travel-map/src/components/GoogleMapMarker.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+  props: {
+    google: {
+      type: Object,
+    },
+    map: {
+      type: Object,
+    }
+  },
+
+  methods:{
+    makemarker(data, is_first) {
+      const marker = new this.google.maps.Marker({
+        position: data.position,
+        map: this.map,
+        icon: {
+              url: 'http://maps.google.com/mapfiles/ms/micons/cabs.png',
+        },
+      });
+      if (is_first){
+        this.map.setZoom(12);
+        this.map.panTo(data.position);
+      }
+      marker.addListener('mouseover',function(){
+        this.map.panTo(data.position);
+      });
+    }
+  }
+}
+</script>
